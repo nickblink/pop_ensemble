e2366d6faef5a200201cf443d0bc600a9d2af612
Nick Link <nicklink415@gmail.com>
2022-08-05 17:39:08 -0400

    Nick's Addition to data extraction and exploration

--- code/02_explore_data.R
+++ code/02_explore_data.R
@@ -2,7 +2,7 @@
 ### Will update this later to include Worldpop and FB (though I'll have to do a later year for FB at least)
 library(ggplot2)
 
-current_path <- getActiveDocumentContext()$path
+current_path <- rstudioapi::getActiveDocumentContext()$path
 setwd(dirname(current_path))
 
 load('../pop_data/merged_denom_cov_data2206.RData')
@@ -12,6 +12,7 @@ cor(adat[,3:5])
 
 # looks like there is some bias
 colMeans(adat[,3:5])
+colMeans(adat[,3:5])/mean(adat$census)
 
 # plotting bias and such
 adat$acs_bias = adat$acs - adat$census
@@ -21,6 +22,7 @@ adat$pep_bias = adat$pep - adat$census
 adat$pep_prop_bas = adat$pep_bias/adat$census
 
 adat$sd = apply(adat, 1, function(xx) sd(xx[3:5]))
+adat$var = apply(adat, 1, function(xx) var(xx[3:5]))
 
 # ggplot(data = adat) + 
 #   geom_point(aes(x = census, y = acs_bias)) +
@@ -42,3 +44,34 @@ ggplot(data = adat) +
   scale_x_continuous(trans='log2') +
   scale_y_continuous(trans='log2')
 
+ggplot(data = adat) +  
+  geom_point(aes(x = census, y = var))  +
+  geom_abline(intercept = 0, slope = 1, color = 'red') +
+  scale_x_continuous(trans='log2') +
+  scale_y_continuous(trans='log2') +
+  ylab('Var(PEP, ACS, census)') +
+  xlab('census count') +
+  ggtitle('variance and census mean by county')
+
+tt = adat %>%
+  arrange(1-var)
+
+acs_prop_bias <- sum(adat$acs)/sum(adat$census)
+pep_prop_bias <- sum(adat$pep)/sum(adat$census)
+
+adat_unbias <- adat %>%
+  mutate(acs = acs/acs_prop_bias,
+         pep = pep/pep_prop_bias)
+adat_unbias$var = apply(adat_unbias, 1, function(xx) var(xx[3:5]))
+
+ggplot(data = adat_unbias) +  
+  geom_point(aes(x = census, y = var))  +
+  geom_abline(intercept = 0, slope = 1, color = 'red') +
+  scale_x_continuous(trans='log2') +
+  scale_y_continuous(trans='log2') +
+  ylab('Var(PEP, ACS, census)') +
+  xlab('census count') +
+  ggtitle('variance after bias adjusting')
+
+tt = adat_unbias %>%
+  arrange(1-var)
