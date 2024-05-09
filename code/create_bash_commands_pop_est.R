36823dab80e71ee3377a51ce3f3271b443acbd02
Nick Link <nicklink415@gmail.com>
2024-05-09 18:16:07 -0400

    FALSE to F

--- code/create_bash_commands_pop_est.R
+++ code/create_bash_commands_pop_est.R
@@ -13,7 +13,9 @@ bash_command <- function(N_sims=10, dataset='NY', N_models=3, n.sample=10000, bu
   params <- list(N_sims=N_sims, dataset=dataset, N_models=N_models, n.sample=n.sample, burnin=burnin, family=family,use_softmax=F,variances=variances, means=means, rho = rho, tau2 = tau2, sigma2 = sigma2, sigma2_prior_shape = sigma2_prior_shape, sigma2_prior_rate = sigma2_prior_rate, tau2_prior_shape = tau2_prior_shape, tau2_prior_rate=tau2_prior_rate, num_y_samples=num_y_samples, stan_path=stan_path, CV_blocks = CV_blocks, return_quantiles = return_quantiles, output_path = output_path)
   
   param_str = paste(paste(names(params), params, sep = '='), collapse=':') %>%
-    gsub(' |c\\(|\\)','',.)
+    gsub(' |c\\(|\\)','',.) %>%
+    gsub('TRUE','T',.) %>%
+    gsub('FALSE','F',.)
 
   command_str = sprintf('sbatch --array=1-10 -J %s run_sim_pop_est.bash %s', job_name, param_str)
   
