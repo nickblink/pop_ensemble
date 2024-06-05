51beb2cbf7dbe026a0685cbf44a7e7863effc402
Thinkpad E15 <nicklink415@gmail.com>
2024-06-05 08:20:59 -0400

    updating bash commands

--- code/create_bash_commands_pop_est.R
+++ code/create_bash_commands_pop_est.R
@@ -1,7 +1,7 @@
 library(dplyr)
 
 ## This script makes bash commands for given simulations
-bash_command <- function(R=40, dataset='NY', N_models=3, n.sample=10000, burnin=5000, family='normal',use_softmax=F,variances=c(100,100,100), means=c(100,100,100), rho = 0.3, tau2 = .01, sigma2 = 100, sigma2_prior_shape = 50, sigma2_prior_rate = 0.5, tau2_prior_shape = 1, tau2_prior_rate=1, num_y_samples=3, stan_path='code/CAR_leroux_sparse_normal.stan', CV_blocks = 5, return_quantiles = T,parallel = T, output_path_addition = NULL, array_length = 5){
+bash_command <- function(R=40, dataset='NY', N_models=3, n.sample=10000, burnin=5000, family='normal',use_softmax=F,variances=c(100,100,100), means=c(100,100,100), rho = 0.3,  rho_value = -1, tau2 = .01, tau2_value = -1, sigma2 = 100, sigma2_prior_shape = 50, sigma2_prior_rate = 0.5, tau2_prior_shape = 1, tau2_prior_rate=1, num_y_samples=3, stan_path='code/CAR_leroux_sparse_normal.stan', CV_blocks = 5, return_quantiles = T,parallel = T, output_path_addition = NULL, array_length = 5){
   
   if(!use_softmax & tau2 > 0.1){
     print('Are you sure you want tau2 so high?')
@@ -16,9 +16,9 @@ bash_command <- function(R=40, dataset='NY', N_models=3, n.sample=10000, burnin=
     output_path <- gsub('\\.','',output_path)
   }
   
-  job_name = sprintf('%s_%s_%smodels_CV%s', ifelse(use_softmax, 'softmax', 'directest'), family, N_models, CV_blocks)
+  job_name = sprintf('%s_%s_%s_%smodels_CV%s', round(runif(1)*1000), ifelse(use_softmax, 'softmax', 'directest'), family, N_models, CV_blocks)
   
-  params <- list(R=R, dataset=dataset, N_models=N_models, n.sample=n.sample, burnin=burnin, family=family,use_softmax=use_softmax,variances=variances, means=means, rho = rho, tau2 = tau2, sigma2 = sigma2, sigma2_prior_shape = sigma2_prior_shape, sigma2_prior_rate = sigma2_prior_rate, tau2_prior_shape = tau2_prior_shape, tau2_prior_rate=tau2_prior_rate, num_y_samples=num_y_samples, stan_path=stan_path, CV_blocks = CV_blocks, return_quantiles = return_quantiles, parallel = parallel, output_path = output_path)
+  params <- list(R=R, dataset=dataset, N_models=N_models, n.sample=n.sample, burnin=burnin, family=family,use_softmax=use_softmax,variances=variances, means=means, rho = rho, rho_value = rho_value, tau2 = tau2, tau2_value = tau2_value, sigma2 = sigma2, sigma2_prior_shape = sigma2_prior_shape, sigma2_prior_rate = sigma2_prior_rate, tau2_prior_shape = tau2_prior_shape, tau2_prior_rate=tau2_prior_rate, num_y_samples=num_y_samples, stan_path=stan_path, CV_blocks = CV_blocks, return_quantiles = return_quantiles, parallel = parallel, output_path = output_path)
   
   param_str = paste(paste(names(params), params, sep = '='), collapse=':') %>%
     gsub(' |c\\(|\\)','',.) %>%
@@ -32,9 +32,14 @@ bash_command <- function(R=40, dataset='NY', N_models=3, n.sample=10000, burnin=
 }
 
 
-bash_command(CV_blocks = 10, tau2 = 0.01, rho = 0.3, rho_value = 0.99, output_path_addition = 'fixedrho_rho03')
+bash_command(CV_blocks = 10, tau2 = 0.01, rho = 0.3, rho_value = 0.99, output_path_addition = 'fixedrho099_rho03')
 
-WRITE OUT THE REST OF THESE AND SIMULATE DEM YO.
-MAKE SURE TO TRANSFER OVER NEW FILES.
+bash_command(CV_blocks = 10, tau2 = 0.01, rho = 0.7, rho_value = 0.99, output_path_addition = 'fixedrho099_rho07')
 
-# bash_command(CV_blocks = 0, tau2 = 1, use_softmax = T, rho = 0.99, output_path_addition = 'rho099')
+bash_command(CV_blocks = 10, tau2 = 0.01, rho = 0.99, rho_value = 0.99, output_path_addition = 'fixedrho099_rho099')
+
+bash_command(CV_blocks = 10, use_softmax = T, tau2 = 1, rho = 0.3, rho_value = 0.99, output_path_addition = 'fixedrho099_rho03')
+
+bash_command(CV_blocks = 10, use_softmax = T, tau2 = 1,  rho = 0.7, rho_value = 0.99, output_path_addition = 'fixedrho099_rho07')
+
+bash_command(CV_blocks = 10, use_softmax = T, tau2 = 1, rho = 0.99, rho_value = 0.99, output_path_addition = 'fixedrho099_rho099')
